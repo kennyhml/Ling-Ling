a6fa478e75ea6f425710a967d081b43c9bbb5837
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-01-23 09:14:26 +0100

    increased sap delay

--- LLPP/src/bots/kitchen/sapstation.cpp
+++ LLPP/src/bots/kitchen/sapstation.cpp
@@ -52,6 +52,6 @@ namespace llpp::bots::kitchen
         asa::core::sleep_for(std::chrono::seconds(2));
         storage_box_slots_ = storage_box_.get_current_slots();
         asa::entities::local_player->get_inventory()->close();
-        asa::core::sleep_for(std::chrono::seconds(1));
+        asa::core::sleep_for(std::chrono::seconds(2));
     }
 }
