6ffd2984fcee6fd3c73bede8648506951da9f444
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-02-17 13:48:30 +0100

    added embed factory

--- LLPP/src/discord/embeds.cpp
+++ LLPP/src/discord/embeds.cpp
@@ -1,11 +1,10 @@
 #include "embeds.h"
 #include "icons.h"
+#include "helpers.h"
 #include "../config/config.h"
 #include "../common/util.h"
 #include <asapp/game/settings.h>
 #include <asapp/game/window.h>
-
-#include "helpers.h"
 
 
 namespace llpp::discord
@@ -32,7 +31,8 @@ namespace llpp::discord
         embed.set_title("Ling Ling++ has crashed!");
         embed.set_color(dpp::colors::red);
         embed.set_thumbnail(REPAIR_RED_ICON);
-        embed.set_description("Encountered an unexpected error - manual restart required.");
+        embed.set_description(
+            "Encountered an unexpected error - manual restart required.");
 
         embed.add_field("Error: ", error.what(), true);
         embed.add_field("Task: ", task, true);
@@ -42,15 +42,17 @@ namespace llpp::discord
         return embed;
     }
 
-    dpp::embed get_station_disabled_embed(const std::string& station, const std::string& reason)
+    dpp::embed get_station_disabled_embed(const std::string& station,
+                                          const std::string& reason)
     {
         dpp::embed embed;
 
-        embed.set_thumbnail(REPAIR_BROWN_ICON);
         embed.set_title(std::format("'{}' has been disabled automatically!", station));
+        embed.set_color(dpp::colors::beige);
+        embed.set_thumbnail(REPAIR_BROWN_ICON);
         embed.set_description(
             "Please resolve the issue and re-enable the station.\n"
-            "You can do via discord commands or by restarting the bot.");
+            "You can do so via discord commands or by restarting the bot.");
         embed.add_field("Station: ", station, true);
         embed.add_field("Reason:", reason, true);
 
@@ -58,5 +60,24 @@ namespace llpp::discord
         return embed;
     }
 
+    dpp::embed get_station_suspended_embed(const std::string& station,
+                                           const std::string& reason,
+                                           const std::chrono::minutes duration)
+    {
+        dpp::embed embed;
 
+        embed.set_title(std::format("'{}' has been suspended!", station));
+        embed.set_color(dpp::colors::brown);
+        embed.set_thumbnail(TIME_GLASS_ICON);
+        embed.set_description(
+            "Completion will not be attempted while suspended.\n"
+            "You can lift the suspension by re-enabling the station via command.");
+
+        embed.add_field("Station: ", station, true);
+        embed.add_field("Reason:", reason, true);
+        embed.add_field("Duration: ", std::format("{} seconds", duration.count()), true);
+
+        set_now_timestamp(embed);
+        return embed;
+    }
 }
