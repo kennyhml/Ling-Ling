07b538112b08241faf042d2871a5bef933315831
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-12-29 23:43:47 +0100

    docs(discord): document command factories

--- bot/src/integrations/discord/commands/config.h
+++ bot/src/integrations/discord/commands/config.h
@@ -3,5 +3,11 @@
 
 namespace discord
 {
+    /**
+     * @brief Creates the /config command group designed to to allow the user to change
+     * the configuration files. May register multiple subcommands for ease of use.
+     *
+     * @return The slashcommand and the function to handle the command being triggered.
+     */
     [[nodiscard]] slashcommand_and_callback_t create_config_command();
 }
--- bot/src/integrations/discord/commands/setup.h
+++ bot/src/integrations/discord/commands/setup.h
@@ -3,5 +3,11 @@
 
 namespace discord
 {
+    /**
+     * @brief Creates the /setup command group designed to handle the setup of a
+     * category and channel with the relevant permissions.
+     *
+     * @return The slashcommand and the function to handle the command being triggered.
+     */
     [[nodiscard]] slashcommand_and_callback_t create_setup_command();
 }
