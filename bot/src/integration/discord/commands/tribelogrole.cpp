d863d6b66c9e7024657970ea709a7a33ecc70de4
Caleb <69630673+Caleb106@users.noreply.github.com>
2025-01-02 13:35:20 +0000

    Update tribelogrole.cpp

--- bot/src/integration/discord/commands/tribelogrole.cpp
+++ bot/src/integration/discord/commands/tribelogrole.cpp
@@ -4,12 +4,6 @@ namespace lingling::discord {
 
     void listener()
     {
-        std::cout << "start" << "\n";
         add_setup_command_create_listener(triberole);
-        std::cout << "end" << "\n";
-    }
-    void start_listener()
-    {
-        listener();
     }
 }
